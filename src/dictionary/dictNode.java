119d688612357bb2a5c8bda56eb9c74186a0f263
chetana.nagare <chetananagare12@gmail.com>
2022-02-10 10:42:35 +0530

    first commit

---
+++ src/dictionary/dictNode.java
@@ -0,0 +1,18 @@
+package dictionary;
+public class dictNode {
+    dictNode left, right;
+    int h;
+    String word;
+    String mean;
+    String type; //whether word is adjective/noun/verb
+    String synonym;
+
+    dictNode(String w, String m, String tp, String syn){
+        word = w;
+        mean = m;
+        type = tp;
+        synonym = syn;
+        h = 0;
+        left = right = null;
+    }
+}
